8e3427f87f95406d4bb8a4e692b70fb23c1ed8d4
Akiyama <nhatvu148@gmail.com>
2021-01-30 15:05:51 +0900

    file loader, clean

--- webpack.common.js
+++ webpack.common.js
@@ -24,6 +24,20 @@ module.exports = {
         exclude: /node_modules/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "img",
+          },
+        },
+      },
     ],
   },
 };
