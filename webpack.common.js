d493e01bd87dcbcea95ca87edf8d24a49a3f4222
Akiyama <nhatvu148@gmail.com>
2021-01-30 14:52:01 +0900

    separate prod dev

--- webpack.common.js
+++ webpack.common.js
@@ -2,23 +2,13 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  entry: "./index.js",
-  output: {
-    filename: "main.[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
-  },
+  entry: "./src/index.js",
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
     }),
   ],
   target: "node",
-  devServer: {
-    port: "3300",
-    contentBase: ["./public"],
-    open: true,
-  },
   resolve: {
     extensions: [".js", ".jsx", ".json"],
   },
---
+++ webpack.dev.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const common = require("./webpack.common");
+const { merge } = require("webpack-merge");
+
+module.exports = merge(common, {
+  mode: "development",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    port: "3300",
+    contentBase: ["./dist"],
+    open: true,
+  },
+});
